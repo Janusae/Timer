3883a254e310271f0c05371732ae6a23c14bbec5
Janusae <147909135+Janusae@users.noreply.github.com>
2023-11-25 10:29:42 +0330

    add last feature

--- change.py
+++ change.py
@@ -16,15 +16,22 @@ time = [ask_hour,ask_minute,ask_second,10000]
 while True:
           while True:
                     time[3] -= 1
-                    print(time)
-                    if time[3] == 0:
+                    
+                    if time[3] == 0 and time[2] != 0:
                               time[3] = 10000
-                              time[2] -=1
-                    print(time)
-                    if time[2] == 0:
+                              time[2] -= 1 
+                    elif time[2] == 0 and time[1] == 0 and time[0] == 0:
+                              break
+                    elif time[2] == 0 and time[1] != 0:
                               time[2] = 59
                               time[1] -= 1
-                    if time[1] == 0:
+                    elif time[2] == 0 and time[1] == 0:
+                              time[2] = 59
                               time[1] = 59
                               time[0] -= 1
-                              
+                    print(F"{time[0]}:{time[1]}:{time[2]}")
+          break
+import winsound
+file_name = "telephone-ring-02.wav"
+winsound.PlaySound(file_name , winsound.SND_FILENAME)
+
